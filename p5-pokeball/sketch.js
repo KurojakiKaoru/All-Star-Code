683872a37127e0389762b777a093a9f0350e80ec
LeronDoesGaming <leronhinds@gmail.com>
2018-07-15 02:27:02 -0400

    Pokeball

---
+++ p5-pokeball/sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(300,300);
+    background(200);
+    strokeWeight(5);
+    fill(255,0,0);
+    ellipse(150,150,250,250);
+    fill(255);
+    arc(150, 150, 250, 250, 0, PI, CHORD);
+    strokeWeight(5);
+    fill(255);
+    ellipse(150,150,50,50);
+    fill(0);
+    ellipse(150,150,25,25)
+}
