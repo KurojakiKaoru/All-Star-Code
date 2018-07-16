414d51f263c14e5dc96e92bdeefe93bcf0be9340
LeronDoesGaming <leronhinds@gmail.com>
2018-07-16 17:04:32 -0400

    Simplified the colors for better understanding

--- p5-pokeball/sketch.js
+++ p5-pokeball/sketch.js
@@ -1,14 +1,20 @@
 function setup(){
-    createCanvas(300,300);
+    createCanvas(300, 300);
     background(200);
     strokeWeight(5);
-    fill(255,0,0);
-    ellipse(150,150,250,250);
-    fill(255);
+    fill('red');
+    ellipse(150, 150, 250, 250);
+    fill('white');
     arc(150, 150, 250, 250, 0, PI, CHORD);
     strokeWeight(5);
-    fill(255);
+    fill('white');
     ellipse(150,150,50,50);
-    fill(0);
+    fill('black');
     ellipse(150,150,25,25)
 }
+
+//function mouseDragged() {
+    //if(mouseX == 150 && mouseY = 150) {
+
+    //}
+//}
